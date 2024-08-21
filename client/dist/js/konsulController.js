5eb5bf30b6e4b9527b53fa364b93ca602f7b5ffb
victorown <abraham2001ohoiwutun@gmail.com>
2024-08-21 15:48:01 +0900

    so far so good

--- client/dist/js/konsulController.js
+++ client/dist/js/konsulController.js
@@ -17,17 +17,32 @@ new Vue({
         busy: true,
         hasils: [],
         result: false,
+        showRes: false,
+        showGuide: true,
+        imgUrl: '',
+        showToTopButton: false,
         inputStyle: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight invalid:focus:outline-red-400 valid:focus:outline-blue-500 mt-2",
     },
-    mounted() {
-        this.idKonsumen = localStorage.getItem("konsumen")
-        if (this.idKonsumen) {
-            this.showForm = true
-        } else {
-            this.showForm = false
+    watch: {
+        showRes(newVal) {
+            console.log("showRes changed:", newVal);
+            if (newVal) {
+                this.$nextTick(() => {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                });
+            }
         }
     },
-    created() {
+    mounted() {
+        window.addEventListener('scroll', this.scrollFunction);
+        this.idKonsumen = localStorage.getItem("konsumen");
+        this.showForm = !!this.idKonsumen;
+    },
+    beforeDestroy() {
+        window.removeEventListener('scroll', this.toggleVisibility);
     },
     methods: {
         submitFormData() {
@@ -50,7 +65,6 @@ new Vue({
 
         async getIdKondisi(kode) {
             const idKondisi = await axios.get(`${this.apiUrl}/kondisi/kode/${kode}`);
-            console.log("id Kondisi: ", idKondisi.data.id);
             return idKondisi.data.id
         },
 
@@ -66,7 +80,6 @@ new Vue({
                     pilihan: answersPilihan,
                 });
             }
-            console.log('masukan data: ', this.Jawaban);
         },
 
         async submitAnswers() {
@@ -85,22 +98,21 @@ new Vue({
                     this.Jawaban = this.Jawaban.filter(jawaban => jawaban !== undefined);
                 }
 
-                console.log("data answer final: ", JSON.stringify(this.Jawaban));
-
             } catch (error) {
                 console.error(error);
             }
 
-            let data = this.Jawaban.map(x => {return {pilihan: x.pilihan, code: x.code}})
-            this.mapingHasils();
-            console.log("ini isi data: ", data);
-            console.log("Maping 1st: ", this.hasils);
+            let data = this.Jawaban.map(x => { return { pilihan: x.pilihan, code: x.code } })
+            // this.mapingHasils();
             axios.post(this.apiUrl + '/hitung', data, this.header)
                 .then(response => {
                     this.hasils.final = response.data;
+                    this.hasils.final.sort((a, b) => b.odd - a.odd);
                     console.log('Response:', response.data);
-                    console.log('Maping 2nd:', this.hasils);
                     // this.submitJawabans(this.Jawaban);
+                    this.mapingHasils();
+                    this.setFirstRecommend(this.hasils.final);
+                    this.showRes = true;
                 })
                 .catch(error => {
                     console.error('Error:', error);
@@ -109,15 +121,35 @@ new Vue({
 
         async submitJawabans(model) {
             let data = {};
-            data.pilihan = model.map(x => {return {pilihan: x.pilihan, KondisiId: x.kondisiId }});
+            data.pilihan = model.map(x => { return { pilihan: x.pilihan, KondisiId: x.kondisiId } });
             data.konsumenId = this.idKonsumen;
             axios.post(this.apiUrl + '/konsul', data, this.header);
         },
 
-        mapingHasils(){
-            this.hasils.pilihan = this.Jawaban.map(x => {return {pilihan: x.pilihan}});
-            this.hasils.kondisi = this.questions.map(x => {return {question: x.text}});
+        mapingHasils() {
+            let pilihanx = this.Jawaban.map(x => x.pilihan);
+            let questionx = this.questions.map(x => x.text);
+            this.hasils.selected = pilihanx.map((pilihans, index) => { return { pilihans, questions: questionx[index] } });
         },
 
-    }
+        formatPercentage(num) {
+            return new Intl.NumberFormat('default', {
+                style: 'percent',
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            }).format(num);
+        },
+
+        setFirstRecommend(data) {
+            this.firstRecommend = data[0];
+            console.log("first index of final arrays: ", this.firstRecommend);
+            if (this.firstRecommend.cat.nama === "Colour Extreme") this.imgUrl = "../dist/img/colour-extreme.png";
+            else if (this.firstRecommend.cat.nama === "Ultra Clean") this.imgUrl = "../dist/img/ultra-clean.png";
+            else if (this.firstRecommend.cat.nama === "Flex") this.imgUrl = "../dist/img/flex.png";
+            else if (this.firstRecommend.cat.nama === "Antifade") this.imgUrl = "../dist/img/antifade.png";
+            else if (this.firstRecommend.cat.nama === "Tough Shield") this.imgUrl = "../dist/img/tough-shield.png";
+            else if (this.firstRecommend.cat.nama === "Tough Shield Max") this.imgUrl = "../dist/img/tough-shield-max.png";
+        },
+
+    },
 });
